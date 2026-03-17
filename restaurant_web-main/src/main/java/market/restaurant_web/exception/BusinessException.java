663bf50129c37b9ab0420fa257570543ac80195e
ductran2005 <ductrancode@gmail.com>
2026-03-17 17:48:56 +0700

    del

--- restaurant_web-main/src/main/java/market/restaurant_web/exception/BusinessException.java
+++
@@ -1,7 +0,0 @@
-package market.restaurant_web.exception;
-
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
